5739a6cae8112a2646fd0c31dbef2bba2cc25efe
chisthian29 <christhian29@gmail.com>
2015-07-25 13:51:28 -0600

    https://github.com/RLC-Crew/CGP.git

---
+++ CGP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CGP.Startup))]
+namespace CGP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
